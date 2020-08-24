e055f1f7c0c2c04ad0691ec4839b70149f4fa137
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-24 17:41:11 -0400

    created directory

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const util = require('util');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Ivan0825*',
+    database: 'employees'
+});
+
+connection.connect();
+
+connection.query = util.promisify(connection.query);
+module.exports = connection;
