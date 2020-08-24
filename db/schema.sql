4a486ce790c4a7950fa4468ad3ac2d73525ea67c
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-24 18:40:47 -0400

    edited schem.sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,21 +3,25 @@ CREATE DATABASE employee_DB;
 USE employee_DB;
 
 CREATE TABLE department(
-id INT PRIMARY KEY,
-department VARCHAR(30) NOT NULL
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+department VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE employee_role (
-id INT PRIMARY KEY NOT NULL,
-title VARCHAR(30) NOT NULL,
-salary DECIMAL(10,2) NOT NULL,
-department_id INT 
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30) UNIQUE NOT NULL,
+salary DECIMAL(10,2) UNSIGNED NOT NULL,
+INDEX dep_ind(department_id),
+-- CONSTRAINT????
 );
 
 CREATE TABLE employee (
-id INT PRIMARY KEY NOT NULL,
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL,
-manager_id INT
+INDEX role_ind(role_id),
+-- CONSTRAINT????
+INDEX manager_ind(manager_id),
+-- CONSTRAINT????
 )
